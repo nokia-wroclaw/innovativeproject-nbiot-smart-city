636e080d88a75e77675d57981b1ebb7868261b86
Daniel Łobacz <229363@student.pwr.edu.pl>
2019-06-09 20:13:07 +0200

    little cleanup and rename

--- CodeSamples/mqtt2rest.py
+++ CodeSamples/mqtt2rest.py
@@ -18,15 +18,10 @@ def mqtt_fun():
     specificTopic   = cred.mqtt['token'][tokenID] + "/urn:sku:meter-001/temperature/0/sensorValue"
     generalTopic    = cred.mqtt['token'][tokenID] + "/#"
 
-    # The callback for when the client receives a CONNACK response from the server.
     def on_connect(client, userdata, flags, rc):
         print("Connected with result code "+str(rc))
-
-        # Subscribing in on_connect() means that if we lose the connection and
-        # reconnect then subscriptions will be renewed.
         client.subscribe(generalTopic)
 
-    # The callback for when a PUBLISH message is received from the server.
     def on_message(client, userdata, msg):
         split = msg.topic.split("/")
         name = split[2]
@@ -39,7 +34,6 @@ def mqtt_fun():
     client.on_message = on_message
 
     client.username_pw_set(cred.mqtt['login'][tokenID], cred.mqtt['password'][tokenID])
-
     client.connect(cred.impact['url'], cred.impact['port'], 60)
 
     client.loop_forever()
